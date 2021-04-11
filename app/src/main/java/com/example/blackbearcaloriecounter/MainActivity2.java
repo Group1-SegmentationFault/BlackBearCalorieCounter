5ec5a75a2f09a44ab662c38e3b79afb148cd6818
travistovey <79064030+travistovey@users.noreply.github.com>
2021-04-11 09:19:50 -0400

    functional bottom navigation buttons

--- app/src/main/java/com/example/blackbearcaloriecounter/MainActivity2.java
+++ app/src/main/java/com/example/blackbearcaloriecounter/MainActivity2.java
@@ -1,10 +1,15 @@
 package com.example.blackbearcaloriecounter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.example.blackbearcaloriecounter.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -13,8 +18,36 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        //homeFragment = HomeFragment();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.home_icon:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.calorie_icon:
+                            selectedFragment = new CaloriesFragment();
+                            break;
+                        case R.id.settings_icon:
+                            selectedFragment = new SettingsFragment();
+                            break;
+
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.bottom_navigation,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
 
 }
